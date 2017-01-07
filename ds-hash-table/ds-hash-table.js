5e8ecf8f4e0a6b42d8135a67b12df83555af5909
DaniaHamdan <dandonzh@gmail.com>
2017-01-07 10:18:21 +0200

    hash

--- ds-hash-table/ds-hash-table.js
+++ ds-hash-table/ds-hash-table.js
@@ -1,17 +1,35 @@
 var makeHashTable = function() {
   //Do not change the max!
   var max = 4;
-
+  var flag=true;
   return {
     _storage: [],
     retrieve: function(key) {
-      //your code is here
+      var index=hashFn(key,max)
+      for (var i = 0; i <_storage[index].length; i++) {
+             if(_storage[index][i][0]===key){
+             return _storage[index][i][1];
+              
+             }
+           } 
     },
-
     insert: function(key, value) {
-      //your code is here
+      var index=hashFn(key,max);
+      if(_storage[index]){
+        _storage[index]=[];
+      }
+      for (var i = 0; i <_storage[index].length; i++) {
+             if(_storage[index][i][0]===key){
+              _storage[index][i][1]===value;
+              flag=false;
+             }
+           } 
+           if(flag){
+            _storage[index].push([key,value])
+           }    
   };
-};
+}
+}
 
 // This is a "hashing function". You don't need to worry about it, just use it to turn any key into a pseudo-random key
 var hashFn = function(str, max) {
